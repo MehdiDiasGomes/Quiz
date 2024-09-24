114772f494f7b8996facd00b64f68130ef4c6628
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2024-09-24 23:04:28 +0200

    ✨ (Feat): Add naruto quiz

--- src/Pages/Home.vue
+++ src/Pages/Home.vue
@@ -5,7 +5,7 @@
       <img
         class="w-full h-full object-cover opacity-35 rounded-lg group-hover:opacity-70 group-hover:scale-105 duration-300"
         src="../assets/snk_wallpaper.jpg"
-        alt="snk"
+        alt="Attaques des titans"
       />
 
       <h3 class="absolute text-2xl bottom-5 left-5 text-white font-bold">Attaques des titans</h3>
@@ -15,9 +15,18 @@
       <img
         class="w-full h-full object-cover rounded-lg opacity-35 group-hover:opacity-70 group-hover:scale-105 duration-300"
         src="../assets/bleach_wallpaper.jpg"
-        alt="snk"
+        alt="Bleach"
       />
       <h3 class="absolute text-2xl bottom-5 left-5 text-white font-bold">Bleach</h3>
+    </RouterLink>
+
+    <RouterLink to="/quiz/naruto" class="w-[20rem] h-[20rem] p-3 gap-5 relative group">
+      <img
+        class="w-full h-full object-cover rounded-lg opacity-35 group-hover:opacity-70 group-hover:scale-105 duration-300"
+        src="../assets/naruto_wallpaper.png"
+        alt="Naruto"
+      />
+      <h3 class="absolute text-2xl bottom-5 left-5 text-white font-bold">Naruto</h3>
     </RouterLink>
   </div>
   </div>
