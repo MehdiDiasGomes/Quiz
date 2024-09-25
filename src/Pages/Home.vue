079ee328eadbdb652edf4b5ffd9f5433f194234f
« MehdiDiasGomes » <diasgomes.mehdi270@gmail.com>
2024-09-25 11:29:40 +0200

    ♻️ (Refact): refact the code

--- src/Pages/Home.vue
+++ src/Pages/Home.vue
@@ -1,60 +1,49 @@
 <template>
   <div class="flex justify-center">
     <div class="w-full flex flex-wrap justify-center mt-10 gap-8">
-    <RouterLink to="/quiz/snk" class="w-[20rem] h-[20rem] p-3 gap-5 relative group">
-      <img
-        class="w-full h-full object-cover opacity-35 rounded-lg group-hover:opacity-70 group-hover:scale-105 duration-300"
-        src="../assets/snk_wallpaper.jpg"
-        alt="Attaques des titans"
-      />
+      <RouterLink to="/quiz/snk" class="w-[20rem] h-[20rem] p-3 gap-5 relative group">
+        <img
+          class="w-full h-full object-cover opacity-35 rounded-lg group-hover:opacity-70 group-hover:scale-105 duration-300"
+          src="../assets/snk_wallpaper.jpg" alt="Attaques des titans" />
 
-      <h3 class="absolute text-2xl bottom-5 left-5 text-white font-bold">Attaques des titans</h3>
-    </RouterLink>
+        <h3 class="absolute text-2xl bottom-5 left-5 text-white font-bold">Attaques des titans</h3>
+      </RouterLink>
 
-    <RouterLink to="/quiz/bleach" class="w-[20rem] h-[20rem] p-3 gap-5 relative group">
-      <img
-        class="w-full h-full object-cover rounded-lg opacity-35 group-hover:opacity-70 group-hover:scale-105 duration-300"
-        src="../assets/bleach_wallpaper.jpg"
-        alt="Bleach"
-      />
-      <h3 class="absolute text-2xl bottom-5 left-5 text-white font-bold">Bleach</h3>
-    </RouterLink>
+      <RouterLink to="/quiz/bleach" class="w-[20rem] h-[20rem] p-3 gap-5 relative group">
+        <img
+          class="w-full h-full object-cover rounded-lg opacity-35 group-hover:opacity-70 group-hover:scale-105 duration-300"
+          src="../assets/bleach_wallpaper.jpg" alt="Bleach" />
+        <h3 class="absolute text-2xl bottom-5 left-5 text-white font-bold">Bleach</h3>
+      </RouterLink>
 
-    <RouterLink to="/quiz/naruto" class="w-[20rem] h-[20rem] p-3 gap-5 relative group">
-      <img
-        class="w-full h-full object-cover rounded-lg opacity-35 group-hover:opacity-70 group-hover:scale-105 duration-300"
-        src="../assets/naruto_wallpaper.png"
-        alt="Naruto"
-      />
-      <h3 class="absolute text-2xl bottom-5 left-5 text-white font-bold">Naruto</h3>
-    </RouterLink>
+      <RouterLink to="/quiz/naruto" class="w-[20rem] h-[20rem] p-3 gap-5 relative group">
+        <img
+          class="w-full h-full object-cover rounded-lg opacity-35 group-hover:opacity-70 group-hover:scale-105 duration-300"
+          src="../assets/naruto_wallpaper.png" alt="Naruto" />
+        <h3 class="absolute text-2xl bottom-5 left-5 text-white font-bold">Naruto</h3>
+      </RouterLink>
 
-    <RouterLink to="/quiz/knb" class="w-[20rem] h-[20rem] p-3 gap-5 relative group">
-      <img
-        class="w-full h-full object-cover rounded-lg opacity-35 group-hover:opacity-70 group-hover:scale-105 duration-300"
-        src="../assets/knb_wallpaper.jpg"
-        alt="Kuruko's basket"
-      />
-      <h3 class="absolute text-2xl bottom-5 left-5 text-white font-bold">Kuruko's basket</h3>
-    </RouterLink>
-    
-    <RouterLink to="/quiz/tokyo-ghoul" class="w-[20rem] h-[20rem] p-3 gap-5 relative group">
-      <img
-        class="w-full h-full object-cover rounded-lg opacity-35 group-hover:opacity-70 group-hover:scale-105 duration-300"
-        src="../assets/tokyo-ghoul_wallpaper.jpg"
-        alt="Tokyo Ghoul"
-      />
-      <h3 class="absolute text-2xl bottom-5 left-5 text-white font-bold">Tokyo Ghoul</h3>
-    </RouterLink>
-    <RouterLink to="/quiz/blue-lock" class="w-[20rem] h-[20rem] p-3 gap-5 relative group">
-      <img
-         class="w-full h-full object-cover rounded-lg opacity-35 group-hover:opacity-70 group-hover:scale-105 duration-300"
-        src="../assets/blue-lock_wallpaper.jpg"
-        alt="Blue Lock"
-      />
-      <h3 class="absolute text-2xl bottom-5 left-5 text-white font-bold">Blue Lock</h3>
-    </RouterLink>
+      <RouterLink to="/quiz/knb" class="w-[20rem] h-[20rem] p-3 gap-5 relative group">
+        <img
+          class="w-full h-full object-cover rounded-lg opacity-35 group-hover:opacity-70 group-hover:scale-105 duration-300"
+          src="../assets/knb_wallpaper.jpg" alt="Kuruko's basket" />
+        <h3 class="absolute text-2xl bottom-5 left-5 text-white font-bold">Kuruko's basket</h3>
+      </RouterLink>
+
+      <RouterLink to="/quiz/tokyo-ghoul" class="w-[20rem] h-[20rem] p-3 gap-5 relative group">
+        <img
+          class="w-full h-full object-cover rounded-lg opacity-35 group-hover:opacity-70 group-hover:scale-105 duration-300"
+          src="../assets/tokyo-ghoul_wallpaper.jpg" alt="Tokyo Ghoul" />
+        <h3 class="absolute text-2xl bottom-5 left-5 text-white font-bold">Tokyo Ghoul</h3>
+      </RouterLink>
+
+      <RouterLink to="/quiz/blue-lock" class="w-[20rem] h-[20rem] p-3 gap-5 relative group">
+        <img
+          class="w-full h-full object-cover rounded-lg opacity-35 group-hover:opacity-70 group-hover:scale-105 duration-300"
+          src="../assets/blue-lock_wallpaper.jpg" alt="Blue Lock" />
+        <h3 class="absolute text-2xl bottom-5 left-5 text-white font-bold">Blue Lock</h3>
+      </RouterLink>
+    </div>
   </div>
-  </div>
-  
+
 </template>
