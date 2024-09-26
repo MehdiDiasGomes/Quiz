31bbb03e9a343fd1d081859018efe865c6f3997e
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2024-09-26 22:31:11 +0200

    ✨ (Feat): implement quiz collections

--- src/Pages/Home.vue
+++ src/Pages/Home.vue
@@ -3,54 +3,31 @@
     <nav class="flex flex-col gap-3 items-center md:flex-row">
       <img class="w-20" src="../assets/logo/logo.png" alt="Logo quiz">
       <div class="flex flex-col items-center md:items-start">
-        <h1 class="text-2xl">Quiz Anime Challenge</h1>
-        <h2>Teste tes Connaissances en Anime 🥢</h2>
+        <h1 class="text-2xl bebas-neue">Quiz Anime Challenge</h1>
+        <h2 class="bebas-neue">Teste tes Connaissances en Anime 🥢</h2>
       </div>
     </nav>
     <div class="w-full flex flex-wrap justify-center mt-10 gap-8">
-      <RouterLink to="/quiz/snk" class="w-[20rem] h-[20rem] p-3 gap-5 relative group">
+      <RouterLink v-for="(q, index) in quizs" :to="q.to" class="w-[20rem] h-[20rem] p-3 gap-5 relative group">
         <img
           class="w-full h-full object-cover opacity-35 rounded-lg group-hover:opacity-70 group-hover:scale-105 duration-300"
-          src="../assets/snk_wallpaper.jpg" alt="Attaques des titans" />
-
-        <h3 class="absolute text-2xl bottom-5 left-5 text-white font-bold">Attaques des titans</h3>
-      </RouterLink>
-
-      <RouterLink to="/quiz/bleach" class="w-[20rem] h-[20rem] p-3 gap-5 relative group">
-        <img
-          class="w-full h-full object-cover rounded-lg opacity-35 group-hover:opacity-70 group-hover:scale-105 duration-300"
-          src="../assets/bleach_wallpaper.jpg" alt="Bleach" />
-        <h3 class="absolute text-2xl bottom-5 left-5 text-white font-bold">Bleach</h3>
-      </RouterLink>
-
-      <RouterLink to="/quiz/naruto" class="w-[20rem] h-[20rem] p-3 gap-5 relative group">
-        <img
-          class="w-full h-full object-cover rounded-lg opacity-35 group-hover:opacity-70 group-hover:scale-105 duration-300"
-          src="../assets/naruto_wallpaper.png" alt="Naruto" />
-        <h3 class="absolute text-2xl bottom-5 left-5 text-white font-bold">Naruto</h3>
-      </RouterLink>
-
-      <RouterLink to="/quiz/knb" class="w-[20rem] h-[20rem] p-3 gap-5 relative group">
-        <img
-          class="w-full h-full object-cover rounded-lg opacity-35 group-hover:opacity-70 group-hover:scale-105 duration-300"
-          src="../assets/knb_wallpaper.jpg" alt="Kuruko's basket" />
-        <h3 class="absolute text-2xl bottom-5 left-5 text-white font-bold">Kuruko's basket</h3>
-      </RouterLink>
-
-      <RouterLink to="/quiz/tokyo-ghoul" class="w-[20rem] h-[20rem] p-3 gap-5 relative group">
-        <img
-          class="w-full h-full object-cover rounded-lg opacity-35 group-hover:opacity-70 group-hover:scale-105 duration-300"
-          src="../assets/tokyo-ghoul_wallpaper.jpg" alt="Tokyo Ghoul" />
-        <h3 class="absolute text-2xl bottom-5 left-5 text-white font-bold">Tokyo Ghoul</h3>
-      </RouterLink>
-
-      <RouterLink to="/quiz/blue-lock" class="w-[20rem] h-[20rem] p-3 gap-5 relative group">
-        <img
-          class="w-full h-full object-cover rounded-lg opacity-35 group-hover:opacity-70 group-hover:scale-105 duration-300"
-          src="../assets/blue-lock_wallpaper.jpg" alt="Blue Lock" />
-        <h3 class="absolute text-2xl bottom-5 left-5 text-white font-bold">Blue Lock</h3>
+          :src="`./img/${q.name}_wallpaper.jpg`" :alt="q.title" />
+        <h3 class="absolute text-2xl bottom-5 left-5 text-white font-bold">{{ q.title }}</h3>
       </RouterLink>
     </div>
   </div>
-
 </template>
+
+<script setup>
+import quiz from '@/constants/quiz';
+import { onMounted, ref } from 'vue';
+import { RouterLink } from 'vue-router';
+
+const quizs = ref()
+
+onMounted(() => {
+  quizs.value = quiz;
+  console.log(quizs.value); // to debug, remove later
+})
+
+</script>
---
+++ src/constants/quiz.js
@@ -0,0 +1,34 @@
+const quiz = [
+    {
+        title: 'Shingeki No Kyojin',
+        name: 'snk',
+        to: '/quiz/snk',
+    },
+    {
+        title: 'Naruto',
+        name: 'naruto',
+        to: '/quiz/naruto',
+    },
+    {
+        title: 'Bleach',
+        name: 'bleach',
+        to: '/quiz/bleach',
+    },
+    {
+        title: 'Blue Lock',
+        name: 'blue-lock',
+        to: '/quiz/blue-lock',
+    },
+    {
+        title: 'Kuruko No Baske',
+        name: 'knb',
+        to: '/quiz/knb',
+    },
+    {
+        title: 'Tokyo Ghoul',
+        name: 'tokyo-ghoul',
+        to: '/quiz/tokyo-ghoul',
+    },
+]
+
+export default quiz;
